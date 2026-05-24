52e56eafab136b23eaf562c1b51a832fbf38d649
owjs3901 <owjs3901@gmail.com>
2026-05-24 17:48:44 +0900

    Fix clippy: remove unneeded return in build.rs

--- libs/braillify/build.rs
+++ libs/braillify/build.rs
@@ -11,12 +11,10 @@ fn main() {
 
     // wasm 타겟으로 빌드할 때는 build.rs를 건너뜀
     let target = std::env::var("TARGET").unwrap_or_default();
-    if target.contains("wasm32") {
-        return;
-    }
-
-    #[cfg(windows)]
-    {
-        embed_manifest(new_manifest("Braillify.Braillify")).expect("unable to embed manifest file");
+    if !target.contains("wasm32") {
+        #[cfg(windows)]
+        {
+            embed_manifest(new_manifest("Braillify.Braillify")).expect("unable to embed manifest file");
+        }
     }
 }
